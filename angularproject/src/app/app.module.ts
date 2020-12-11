d11a54deec7c4cbb363360d029f80ccfa40fed9d
Miriam Kaden <miriam.kaden@hpe.com>
2020-12-11 13:30:38 +0100

    router links

--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    NavbarComponent
+    NavbarComponent,
+    ImprintComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'grid', component: GridComponent},
+      {path: 'imprint', component: ImprintComponent},
+      {path: '', redirectTo: '/grid', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
