80bed666ff3931422934c7e6ba47453ca4f87ce8
Miriam Kaden <miriam.kaden@hpe.com>
2020-12-19 22:49:06 +0100

    e2e tests

--- angularproject/e2e/src/app.po.ts
+++ angularproject/e2e/src/app.po.ts
@@ -1,8 +1,8 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  async navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl);
+  navigateTo() {
+    return browser.get('/login');
   }
 
   async getTitleText(): Promise<string> {
