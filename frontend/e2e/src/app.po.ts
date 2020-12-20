5eb18fe5375f3c1b702b725fc80fc1e6f225cd76
Miriam Kaden <miriam.kaden@hpe.com>
2020-12-20 15:18:35 +0100

    updated app.po

--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -4,8 +4,4 @@ export class AppPage {
   navigateTo() {
     return browser.get('/login');
   }
-
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
-  }
 }
