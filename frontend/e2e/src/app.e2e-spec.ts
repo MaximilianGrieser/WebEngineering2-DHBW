f8f717cc1fc7b1b853beb1a66f2376fffc272d79
Miriam Kaden <miriam.kaden@hpe.com>
2020-12-20 14:30:28 +0100

    updated e2e test

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -1,19 +1,18 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, by, element, logging} from 'protractor';
 import {HttpClient} from "@angular/common/http";
 import {async, fakeAsync, waitForAsync} from "@angular/core/testing";
 
 describe('workspace-project App', () => {
   let page: AppPage;
- // modules installieren
   beforeEach(() => {
     page = new AppPage();
   });
 
   it('login with correct credentials, should go to grid page(calendar view)', function () {
     return page.navigateTo().then(value => {
-      return element(by.id('fuserid')).sendKeys('miri').then(() => {
-        return element(by.id('fpassword')).sendKeys('blub').then(() => {
+      return element(by.id('fuserid')).sendKeys('admin').then(() => {
+        return element(by.id('fpassword')).sendKeys('pw').then(() => {
           return element(by.id('flogin')).click().then(() => {
             browser.waitForAngular();
             return browser.getCurrentUrl().then((url) => {
@@ -28,8 +27,8 @@ describe('workspace-project App', () => {
 
   it('login with wrong credentials, should stay at login page', function () {
     return page.navigateTo().then(value => {
-      return element(by.id('fuserid')).sendKeys('miri').then(() => {
-        return element(by.id('fpassword')).sendKeys('sefdfg').then(() => {
+      return element(by.id('fuserid')).sendKeys('admin').then(() => {
+        return element(by.id('fpassword')).sendKeys('wrongpw').then(() => {
           return element(by.id('flogin')).click().then(() => {
             browser.waitForAngular();
             return browser.getCurrentUrl().then((url) => {
